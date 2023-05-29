392178f993224190c6cee9710f1bfbdcc7db4bf0
Popcorn412 <felix44291@gmail.com>
2023-05-29 11:13:03 -0500

    Update: ui, Add: css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="container">
+      <div className="setting">
+        <div className="toolbar">
+          <div>
+            <input className='input' placeholder='Input your APi key.' type="text" />
+            <input className='input' placeholder='Input your custom API domain.' type="text" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <select name="lang_select" id="lang_select">
+            <option value="en(us)">English (American)</option>
+            <option value="en(uk)">English (British)</option>
+            <option value="ko">Korean</option>
+            <option value="zh">Chinese</option>
+          </select>
+        </div>
+        <div className="row">
+          <textarea name="input-form" id="input-form" cols={5}></textarea>
+          <textarea name="input-form" id="input-form" cols={5}></textarea>
+        </div>
+      </div>
     </div>
   );
 }
